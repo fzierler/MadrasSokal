cc03060dc1ec36190415760810c78d5160601f4e
Fabian Zierler <fabian.zierler@a1.net>
2024-06-24 14:20:30 +0100

    compare with effective sample size

--- scripts/plaquette_dial_from_log.jl
+++ scripts/plaquette_dial_from_log.jl
@@ -1,17 +1,31 @@
 using DelimitedFiles
 using MadrasSokal
 using Plots
+using MCMCDiagnosticTools
+using MCMCDiagnostics
 
 f = "/home/fabian/Documents/Physics/Data/DataDiaL/measurements/Lt56Ls32beta6.45mf0.71mas1.04FUN/out/out_flow"
 f = "/home/fabian/Documents/Physics/Data/DataDiaL/measurements/Lt56Ls32beta6.45mf0.71mas1.04FUN/out/out_plaq_skip4"
+f = "/home/fabian/Documents/Physics/Data/DataDiaL/measurements/Lt56Ls32beta6.45mf0.71mas1.04FUN/out/out_plaq_skip16"
 files  = [f] 
 
 for file in files
     # get data for plaquette and topological charge
     plaq = plaquettes_hirep(file)
-    @show size(plaq)
-
     therm=396
+    therm=99
+
+    # effective sample size from MCMCDiagnostics
+    N  = size(plaq[therm:end])[1]
+    N0 = effective_sample_size(plaq[therm:end])
+    @show N/N0 
+    
+    # effective sample size from MCMCDiagnosticTools
+    N  = size(plaq[therm:end])[1]
+    N0 = ess(plaq[therm:end])
+    @show N/N0 
+
+
     plt2 = autocorrelation_overview(plaq,"<P>",therm;with_exponential=true)
     display(plt2)
 end
