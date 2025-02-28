8ab2897dca263cacfeaff0fca5d19abc3950a929
Fabian Zierler <fabian.zierler@a1.net>
2025-02-28 16:05:15 +0000

    start using hdf5 files as input

--- scripts/topological_charge.jl
+++ scripts/topological_charge.jl
@@ -3,31 +3,33 @@ using MadrasSokal
 using Plots
 using LaTeXStrings
 using DelimitedFiles
+using HDF5
 default(fontfamily="Computer Modern", frame=:box, top_margin=4Plots.mm, left_margin=6Plots.mm, plot_titlefontsize=12)
 
-function topology_plaquette_from_files(files; outdir = "./output/plots",  therm = 1)
+function topology_plaquette_from_files(file; outdir = "./output/plots",  therm = 1)
 
     dir1 = joinpath(outdir,"topology_publication")
     dir2 = joinpath(outdir,"dial_topological_charge")
     ispath(dir1) || mkpath(dir1)
     ispath(dir2) || mkpath(dir2)
 
-    for file in files
-        data = readdlm(file,',';skipstart=1)
-        cfgn, Q = Int.(data[:,1]), data[:,2]
+    f = h5open(file)
+    for ens in keys(f)
+
+        cfgn, Q = read(f[ens],"trajectories"), read(f[ens],"Q")
                 
         plt1,τmax,τexp = MadrasSokal.publication_plot(Q,"Q",therm)
         plt2 = autocorrelation_overview(Q,"Q",therm;with_exponential=true)
         
-        T, L, β, mf,  mas = parse_filename(file)
+        T, L, β, mf,  mas = parse_filename(ens)
         title = latexstring(L"\beta\!=\!%$(β),~ T\!\times\!L^3\!=\!\!%$(T)\!\times\!%$(L)^3,-\!(am_0^{\rm f},am_0^{\rm as})\!=\!(%$(abs(mf)),%$(abs(mas)))")
         plot!(plt1,plot_title=title,size=(800,300))  
         plot!(plt2,plot_title=title)
 
-        savefig(plt1,joinpath(dir1,basename(file)*".pdf"))
-        savefig(plt2,joinpath(dir2,basename(file)*".pdf"))
+        savefig(plt1,joinpath(dir1,ens*".pdf"))
+        savefig(plt2,joinpath(dir2,ens*".pdf"))
     end
 end
 
-files = readdir("./input/DiaL3",join=true) 
-topology_plaquette_from_files(files; therm = 1)
+file = "/home/fabian/Dokumente/Physics/Analysis/MadrasSokal/input/gradient_flow_data.hdf5" 
+topology_plaquette_from_files(file; therm = 1)
