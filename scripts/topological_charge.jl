86ffe236c6b69528318cb2c5ed0f560a5c2a9df8
Fabian Zierler <fabian.zierler@a1.net>
2025-03-02 19:36:23 +0000

    write to csv

--- scripts/topological_charge.jl
+++ scripts/topological_charge.jl
@@ -52,3 +52,25 @@ full_observable_from_hdf5(file,  outfile; label="topology",      name="Q",      
 full_observable_from_hdf5(file,  outfile; label="energy_density",name="energy_density_w0_sym", plot_label=L"\mathcal{E}(w_0)", therm = 1)
 full_observable_from_hdf5(file,  outfile; label="plaquette",     name="plaquette",             plot_label=L"<\!p\!>",          therm = 1)
 full_observable_from_hdf5(fileCB,outfile; label="PS_correlator", name="source_N0_sink_N80/anti TRIPLET g5", index=(:,10), plot_label=L"C_\pi(t=10)", therm = 1)
+
+# Write results into csv file
+fmt(x,Δx,n) = rpad(errorstring(x,Δx)*",",n)
+fmt(x,n)    = rpad(string(round(x,sigdigits=2))*",",n)
+function write_tau_csv(h5file,csv_io)
+    pad1, pad2 = 10, 8
+    f    = h5open(h5file)
+    ens  = keys(f)
+    pad0 = maximum(length.(ens))+2
+    println(csv_io,"$(rpad("name",pad0)) $(rpad("τ_P,",pad1)) $(rpad("τ_exp_P,",pad2)) $(rpad("τ_Q,",pad1)) $(rpad("τ_exp_Q,",pad2)) $(rpad("τ_E,",pad1)) $(rpad("τ_exp_E,",pad2)) $(rpad("τ_Cπ,",pad1)) $(rpad("τ_exp_Cπ,",pad2))")
+    for e in ens    
+        τQ, ΔτQ, τexpQ = read(f[e]["topology"],"tau_int"),       read(f[e]["topology"],"Delta_tau_int"),      read(f[e]["topology"],"tau_exp")
+        τE, ΔτE, τexpE = read(f[e]["energy_density"],"tau_int"), read(f[e]["energy_density"],"Delta_tau_int"),read(f[e]["energy_density"],"tau_exp")
+        τP, ΔτP, τexpP = read(f[e]["plaquette"],"tau_int"),      read(f[e]["plaquette"],"Delta_tau_int"),     read(f[e]["plaquette"],"tau_exp")
+        τπ, Δτπ, τexpπ = read(f[e]["PS_correlator"],"tau_int"),  read(f[e]["PS_correlator"],"Delta_tau_int"), read(f[e]["PS_correlator"],"tau_exp")
+        println(csv_io,"$(rpad(e,pad0)) $(fmt(τP,ΔτP,pad1)) $(fmt(τexpP,pad2)) $(fmt(τQ,ΔτQ,pad1)) $(fmt(τexpQ,pad2)) $(fmt(τE,ΔτE,pad1)) $(fmt(τexpE,pad2)) $(fmt(τπ,Δτπ,pad1)) $(fmt(τexpπ,pad2))")
+    end
+end
+out_csv = "output/tau.csv"
+io = open(out_csv,"w")
+write_tau_csv(outfile,io)
+close(io)
